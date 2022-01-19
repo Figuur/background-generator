c283e21f7e635b7b4565d2bf323d37dbc51575b9
L. Mulder <lmulder_89@hotmail.com>
2022-01-19 12:20:55 +0100

    packeges toegevoegd

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
 var css = document.querySelector("h3");
+console.log('answer')
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
